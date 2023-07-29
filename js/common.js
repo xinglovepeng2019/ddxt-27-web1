82e3b93afccfea6a7496c19280aca05c055cae7f
xinglovepeng2019 <505963847@qq.com>
2023-07-29 22:51:55 +0800

    页面的访问权限

--- js/common.js
+++ js/common.js
@@ -24,6 +24,21 @@ function tip(msg){
   toast.show()
 }
 
-tip(123)
+// tip(123)
 
+
+// 抽取校验函数 (判断是否登录)
+// 访问首页  已登录 ---进入首页    未登录---提示用户--跳转到登录页面
+
+function checkLogin(){
+  const token = localStorage.getItem('user-token')
+  console.log(token,"token")  
+  // token 为null 说明没有
+  if(token ===null){
+    tip("请先登录")
+    setTimeout(()=>{
+    location.href="login.html"
+    },1000)
+  }
+}
 
